05916e583e214ae782fa721a5062827842b18a01
Venice Vargas <venicevargas1@live.com>
2020-09-19 05:05:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -33,9 +33,8 @@ class CashRegister
   end
 
 
-  def void_last_transaction()
-    @total -= @last_transaction_amount
+   def void_last_transaction
+    self.total = self.total - self.last_transaction
   end
 end
-
 
