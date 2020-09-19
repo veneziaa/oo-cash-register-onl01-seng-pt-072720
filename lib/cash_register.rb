9a060249c78886712880f4aac569590e383f2f27
Venice Vargas <venicevargas1@live.com>
2020-09-19 05:08:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,42 +1,37 @@
-class CashRegister
-  attr_accessor :total, :discount, :last_transaction_amount, :items
-  
-  def initialize(discount = 0)
+class CashRegister 
+  attr_accessor :discount, :total
+
+  def initialize(discount=0)
+    @discount = discount 
     @total = 0
-    @discount = discount
     @items = []
-  end
+    @last_item = [] 
+  end 
   
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
-    end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
-  end
-
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
+  def apply_discount
+    if discount != 0 
+      @total -= (@total * (@discount / 100.to_f))
       "After the discount, the total comes to $#{@total.to_i}."
-    else
+    else 
       "There is no discount to apply."
-    end
-  end
-
-
-    def void_last_transaction 
+    end 
+  end 
+  
+  def add_item(title, price, quantity=1)
+    @total += price * quantity 
+    quantity.times do 
+      @items << title 
+    end 
+    @last_item = price * quantity 
+  end 
+  
+  def items 
+    @items 
+  end 
+  
+  def void_last_transaction 
      @items.delete_at(-1)
     self.total = self.total - @last_item
   end 
 end 
 
-list = CashRegister.new(20) 
